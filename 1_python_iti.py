01400ac545d3715afaa94ed8550d885e87b7a155
Mohammed Ashraf <hom9244.9244@gmail.com>
2022-05-14 07:38:41 +0200

    [IMP] 1_python_iti.py:add some notes

--- 1_python_iti.py
+++ 1_python_iti.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3.8
 '''
 Learn:
+    - what is shebang line
     - how print statments 
     - how make comment and multi line comment
     - data type in python and TypeConvention
     - multi line assignment 
 '''
-
+"""
+In many .py files we may see the shebang line on the top of the script.
+Its purpose is to define the location of the interpreter. 
+By adding the line #!/usr/bin/python3 on the top of the script,
+we can run the file.py on a Unix system and automatically will understand that this is a python script.
+Alternative, you could run the script as python3 file.py.
+"""
 # print in python 
 # use ' ' or " " for single line prints 
 # but  ''' ''' for line line statemnt print or """ """
@@ -15,10 +22,10 @@ print('محمد اشرف ')
 print("Hello World  By Muhammed ashraf ")
 print('''Hello World 's Muhammed ashraf 
 fffffffffffffffffffffffffffffffffffffffffffffffffff''')
-# if use 's in world use " " or ''' ''' or use back slash \
+# if use 's in world use " " or ''' ''' or use back slash \ is a escape character in python
 print('this is Muhammed \'s book ')
 
-# comment in python use # for singel line comment
+# commenting in python use #(sharp) for a single line comment
 '''
 this is multi line comment but this is value of string but not assign to variable
 this use for multi line comment
@@ -36,7 +43,7 @@ tuple for Tuple data structure
 NoneType as Null in Other PL use None as x=None
 '''
 # in python we do not declare variable 
-# but when give value we can knew the datatype for variable
+# but when give value, interpreter can recognize it(datatype for variable)
 # variable_name(identifier) = value
 # rules for writing identifier of variable
 '''
@@ -52,7 +59,9 @@ p_bool=True
 x=None
 # to know data type of variable use type(varaible_name)
 # this method of print with variable with use format 
+# % s -> str, d -> int , f -> float, s or d(1,0) -> bool
 print('a_int type is %s and b_float type is %s'%(type(a_int),type(b_float)))
+print('a_int type is %d and b_float type is %f, bool %s'%(a_int,b_float, p_bool))
 print('s_str type is {0} and p_bool type is {1}'.format(type(s_str),type(p_bool)))
 print(f'x type is {type(x)}')
 """
@@ -68,4 +77,5 @@ a,b,c,p=10,256.3,"Muhammed Ahsraf",False
 a_swap,b_swap=10,56
 print("a_swap = ",a_swap," b_swap= ",b_swap) 
 a_swap,b_swap=b_swap,a_swap
+print("a_swap = ",a_swap," b_swap= ",b_swap) 
 print("a_swap = ",a_swap," b_swap= ",b_swap) 
